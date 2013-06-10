ecf7366ff8d898486cb3f3fef6c5e39caab53f3c
qrila <kalle.kuurila@gmail.com>
2013-06-10 19:07:47 +0300

    ui progress

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,16 @@
+#encoding: utf-8
 class GameController < ApplicationController
   def index
   end
+  
+  def draw_word
+  end
+  
+  def check_letter
+  end
+  
+  def change_picture
+  end
+  
   
 end
