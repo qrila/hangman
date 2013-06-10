c9187ba1b6d7d3c0f5ca59f08f68139ae3513b82
Kalle Kuurila <kallekuurila@Kalle-Mac-mini.local>
2013-06-10 11:32:08 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hangman::Application.config.secret_token = 'b2b84c664d27c2fdb380b812b7a5f6b375b6cd53230202a16ca82c8425303b2d98c35f1983c696e4bd81d2f4600ee2319395f1a4e5ebf1beae7f561d755c62c5'
