f09081a0d1e863e5ecec804c7d89d486bf4ab7bd
qrila <kalle.kuurila@gmail.com>
2013-06-13 11:28:35 +0300

    added empty lines for game word characters, taken caren of scandinavian characters, disabled used characters

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,17 +3,19 @@ class GameController < ApplicationController
   def index
   	if (session[:gameword]) != nil
   	  
-  	  if session[:gamestate] < 10
-  	    session[:gamestate] += 1
-  	  end
+      if session[:gamestate] < 10
+        
+      end
   	  
   		@pic = session[:gamestate].to_s + 'hangman.png'
   		@word_to_guess = session[:gameword]
+  		@guessed = session[:guessed_characters]
   		
   	else
   	  session[:gamestate] = 1
   		@pic = 1.to_s + 'hangman.png'
   		@word_to_guess = draw_word
+  		@guessed = session[:guessed_characters] = ''
   		
   	end
   end
@@ -31,6 +33,20 @@ class GameController < ApplicationController
   end
   
   def checkletter
+    guessed_character = params[:id]
+    
+    guessed_character.gsub!('AE', 'Ä')
+    guessed_character.gsub!('OE', 'Ö')
+    guessed_character.gsub!('AO', 'Å')
+    
+    session[:guessed_characters] = session[:guessed_characters] + guessed_character
+    
+    if !(session[:gameword].index(guessed_character))
+      session[:gamestate] = session[:gamestate] + 1
+    else
+      
+    end
+    
     redirect_to game_path
   end
   
@@ -40,12 +56,14 @@ class GameController < ApplicationController
   def start_new_game
     session[:gamestate] = nil
   	session[:gameword] = nil
+  	session[:guessed_characters] = nil
   	redirect_to game_path
   end
   
   def go_back
     session[:gamestate] = nil
   	session[:gameword] = nil
+  	session[:guessed_characters] = nil
   	redirect_to root_path
   end
   
