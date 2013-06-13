723e70275c92f16aab6ae20918f9f7588ccee31e
qrila <kalle.kuurila@gmail.com>
2013-06-13 14:54:20 +0300

    First version ready

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,20 +2,30 @@
 class GameController < ApplicationController
   def index
   	if (session[:gameword]) != nil
-  	  
-      if session[:gamestate] < 10
-        
-      end
-  	  
+  	    	  
   		@pic = session[:gamestate].to_s + 'hangman.png'
   		@word_to_guess = session[:gameword]
   		@guessed = session[:guessed_characters]
+  		
+  		@state = session[:gamestate]
+  		@rights = session[:right_characters]
+  		
+  		@gamelose = 0
+  	  @gamewin = 0
+      if session[:gamestate] > 9
+        @gamelose = 1
+        @gamewin = 0
+      elsif session[:right_characters] == @word_to_guess.length
+        @gamelose = 0
+        @gamewin = 1
+      end
   		
   	else
   	  session[:gamestate] = 1
   		@pic = 1.to_s + 'hangman.png'
   		@word_to_guess = draw_word
   		@guessed = session[:guessed_characters] = ''
+  		session[:right_characters] = 0
   		
   	end
   end
@@ -44,7 +54,9 @@ class GameController < ApplicationController
     if !(session[:gameword].index(guessed_character))
       session[:gamestate] = session[:gamestate] + 1
     else
-      
+      session[:right_characters] != nil ? 
+                  session[:right_characters] = session[:right_characters] + session[:gameword].count(guessed_character) :
+                  session[:right_characters] = session[:gameword].count(guessed_character)
     end
     
     redirect_to game_path
@@ -57,6 +69,7 @@ class GameController < ApplicationController
     session[:gamestate] = nil
   	session[:gameword] = nil
   	session[:guessed_characters] = nil
+  	session[:right_characters] = nil
   	redirect_to game_path
   end
   
@@ -64,6 +77,7 @@ class GameController < ApplicationController
     session[:gamestate] = nil
   	session[:gameword] = nil
   	session[:guessed_characters] = nil
+  	session[:right_characters] = nil
   	redirect_to root_path
   end
   
